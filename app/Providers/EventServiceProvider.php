2e84db129408777823b9ade92d23732ae9feac48
liujialun <liujialun@lf-network.com>
2023-02-13 06:28:09 +0000

    密码重置

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -15,6 +15,10 @@ class EventServiceProvider extends ServiceProvider
      * @var array<class-string, array<int, class-string>>
      */
     protected $listen = [
+
+        \Illuminate\Auth\Events\Verified::class => [
+            \App\Listeners\EmailVerified::class,
+        ],
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],

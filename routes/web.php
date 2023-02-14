023603c5bdad3839960f986d2dc50136a51a59ce
liujialun <liujialun@lf-network.com>
2023-02-14 12:21:01 +0800

    友好SEO

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,9 @@ Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->na
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 

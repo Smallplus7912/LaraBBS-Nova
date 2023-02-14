a4294ac7e0af5b114b48fdc9e8b9113e5c3bcdc5
liujialun <liujialun@lf-network.com>
2023-02-14 08:17:33 +0000

    7.1.代码生成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
+		$this->call(RepliesTableSeeder::class);
         $this->call(TopicsTableSeeder::class);
     }
 }

69575c4fc5c143f5883b56e54dffdf2a121c662c
liujialun <liujialun@lf-network.com>
2023-02-14 13:56:48 +0800

    使用队列

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,13 +3,27 @@
 namespace App\Observers;
 
 use App\Models\Topic;
-use App\Handlers\SlugTranslateHandler;
+use App\Jobs\Translateslug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class TopicObserver
 {
+    // public function saving(Topic $topic)
+    // {
+    //     // XSS 过滤
+    //     //$topic->body = clean($topic->body, 'user_topic_body');
+
+    //     // 生成话题摘录
+    //     $topic->excerpt = make_excerpt($topic->body);
+
+    //     // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+    //     if ( ! $topic->slug) {
+    //         $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+    //     }
+    // }
+
     public function saving(Topic $topic)
     {
         // XSS 过滤
@@ -18,9 +32,15 @@ class TopicObserver
         // 生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
 
+    }
+
+    public function saved(Topic $topic)
+    {
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
         if ( ! $topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
         }
     }
 }

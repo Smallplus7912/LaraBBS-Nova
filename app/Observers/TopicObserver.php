601a3518f512e3a266f70eeefaef3496b3ddcfc7
liujialun <liujialun@lf-network.com>
2023-02-15 09:43:55 +0800

    XSS过滤topic,reply

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -10,24 +10,10 @@ use App\Jobs\Translateslug;
 
 class TopicObserver
 {
-    // public function saving(Topic $topic)
-    // {
-    //     // XSS 过滤
-    //     //$topic->body = clean($topic->body, 'user_topic_body');
-
-    //     // 生成话题摘录
-    //     $topic->excerpt = make_excerpt($topic->body);
-
-    //     // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
-    //     if ( ! $topic->slug) {
-    //         $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
-    //     }
-    // }
-
     public function saving(Topic $topic)
     {
         // XSS 过滤
-        //$topic->body = clean($topic->body, 'user_topic_body');
+        $topic->body = clean($topic->body, 'user_topic_body');
 
         // 生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);

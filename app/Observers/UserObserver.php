2deb464086bfe47188d64cad9405b3801341cf3f
liujialun <liujialun@lf-network.com>
2023-02-17 17:43:26 +0800

    9.6用户默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -9,9 +9,11 @@ use App\Models\User;
 
 class UserObserver
 {
-    public function creating(User $user)
+    public function saving(User $user)
     {
-        //
+        if(empty($user->avatar)){
+            $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
     }
 
     public function updating(User $user)
